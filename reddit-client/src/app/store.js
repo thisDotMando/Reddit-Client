eba34512329595c5f9e13e2edb184c813ac29d12
Michel Kowal <michelkowal@hotmail.de>
2026-01-28 23:26:29 +0000

    add postsReducer to store.js

--- reddit-client/src/app/store.js
+++ reddit-client/src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import filtersReducer from "../features/filters/filtersSlice";
+import postsReducer from " ../features/posts/postsSlice";
 
 export const store = configureStore({
   reducer: {
     filters: filtersReducer,
+    posts: postsReducer,
   },
 });
