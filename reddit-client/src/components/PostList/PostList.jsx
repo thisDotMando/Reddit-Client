0f4338ddf4309900aad9ca2a1900f39d4634cbf6
Michel Kowal <michelkowal@hotmail.de>
2026-01-30 14:09:30 +0000

    Refactor PostList to use Redux state instead of props

--- reddit-client/src/components/PostList/PostList.jsx
+++ reddit-client/src/components/PostList/PostList.jsx
@@ -1,14 +1,22 @@
 import PostCard from "../PostCard/PostCard";
 import "./PostList.css";
+import { useSelector } from "react-redux";
 
-function PostList({ posts }) {
-    if(!posts || posts.length === 0){
-        return <p className="postlist-empty">No posts available.</p>
+function PostList() {
+
+    const posts = useSelector((state) => state.posts.posts);
+
+    const activeFilter = useSelector((state) => state.filters.activeFilter);
+
+    const filteredPosts = posts.filter((post) => post.filterType === activeFilter);  // mockPosts must have an filterType
+
+    if(filteredPosts.length === 0){
+        return <p className="postlist-empty">No posts available.</p> 
     }
 
     return  (
         <section className="post-list">
-            {posts.map((post) => (
+            {filteredPosts.map((post) => (
                 <PostCard 
                 key={post.id}
                 title={post.title}
